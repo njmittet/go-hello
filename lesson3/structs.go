36580392b3154a0a166e89235b6984ffeda75435
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-16 22:43:36 +0100

    Add struct example

---
+++ lesson3/structs.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+type Square struct {
+	X, Y int
+}
+
+// A struct literal denotes a newly allocated struct value by using names fields
+var (
+	s1 = Square{2, 4}  // Has as type Square
+	p1 = &Square{1, 2} // Has as type *Square
+
+	s2 = Square{X: 1} // Y: 0
+	s3 = Square{}     // X: 0, Y: 0
+)
+
+// A struct is a collection of fields
+func main() {
+	fmt.Println(Vertex{12, 24})
+	v := Vertex{21, 42}
+	fmt.Println(v.X, v.Y)
+
+	// Struct fields can be accessed through a struct pointer
+	p := &v
+	p.X = 1e9
+	fmt.Println(v.X)
+
+	fmt.Println(s1, p1, s2, s3)
+	t := *p1
+	fmt.Println(t.X)
+}
