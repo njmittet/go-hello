76edc9c87802e053bd600712edcddcad9593a7a6
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-16 19:21:02 +0100

    Add switch example

--- lesson2/switch.go
+++ lesson2/switch.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+const (
+	OS_X  string = "darwin"
+	LINUX string = "linux"
+)
+
+func main() {
+	fmt.Print("OS is ")
+	switch os := runtime.GOOS; os {
+	case OS_X:
+		fmt.Println("macOS")
+	case LINUX:
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s", os)
+	}
+}
