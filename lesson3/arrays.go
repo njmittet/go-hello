09a457725f37f66c1256b87a8b13c3f2d68e06c4
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-17 19:07:46 +0100

    Add extended slice example

--- lesson3/arrays.go
+++ lesson3/arrays.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// An array has a fixed size.
 func basic() {
 	var a [2]string
 	a[0] = "Go"
@@ -14,13 +15,34 @@ func basic() {
 	fmt.Println(primes)
 }
 
+// A slice is dynamically-sized,
 func slice() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	var slice []int = primes[1:4]
 	fmt.Println(slice)
 }
 
+// A slice just describes a section of an underlying array
+func view() {
+	names := []string{
+		"John",
+		"Ringo",
+		"Paul",
+		"George",
+	}
+	fmt.Println(names)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	// Changing the elements of a slice modifies underlying array
+	b[0] = "Unknown"
+	fmt.Println(a, b)
+	fmt.Println(names)
+}
+
 func main() {
 	basic()
 	slice()
+	view()
 }
