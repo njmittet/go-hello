f605c656326ed00bfd871b20c602b1004a16264b
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-17 18:24:32 +0100

    Add basic slice example

--- lesson3/arrays.go
+++ lesson3/arrays.go
@@ -14,6 +14,13 @@ func basic() {
 	fmt.Println(primes)
 }
 
+func slice() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	var slice []int = primes[1:4]
+	fmt.Println(slice)
+}
+
 func main() {
 	basic()
+	slice()
 }
