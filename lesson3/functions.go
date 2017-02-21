fc58adfb870b375a4ba7acd46892aa205c8759cd
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-21 19:03:59 +0100

    Add closure example

--- lesson3/functions.go
+++ lesson3/functions.go
@@ -10,6 +10,15 @@ func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
+// Returns a function, creating a closure
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 // Functions are first class citizens
 func functionValue() {
 	hypot := func(x, y float64) float64 {
@@ -21,6 +30,18 @@ func functionValue() {
 	fmt.Println(compute(math.Pow))
 }
 
+// A closure is a function value that references variables from outside its body
+func functionClosures() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*1),
+		)
+	}
+}
+
 func main() {
 	functionValue()
+	functionClosures()
 }
