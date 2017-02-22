e44e1b9a0eea513054701c4c952a0dbe16a85907
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-22 20:02:54 +0100

    Extend interface example

--- lesson4/interfaces.go
+++ lesson4/interfaces.go
@@ -10,10 +10,28 @@ type Abser interface {
 	Abs() float64
 }
 
+// Interfaces are implemented implicitly
+type Printer interface {
+	Print()
+}
+
+type Value struct {
+	V string
+}
+
+type T struct {
+	S string
+}
+
 type MyFloat float64
 
 type Vertex struct {
 	X, Y float64
+}
+
+// This method means type MyStruct implements Printer
+func (value Value) Print() {
+	fmt.Println(value.V)
 }
 
 func (f MyFloat) Abs() float64 {
@@ -28,6 +46,11 @@ func (v *Vertex) Abs() float64 {
 }
 
 func main() {
+
+	// A type implements an interface by implementing its methods
+	var printer Printer = Value{"Hello"}
+	printer.Print()
+
 	var a Abser
 	f := MyFloat(-math.Sqrt2)
 	v := Vertex{3, 4}
