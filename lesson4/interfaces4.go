8ed260c50383c6f9c678ead09516e700f44a23d6
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-22 22:57:54 +0100

    Add type switch example

--- lesson4/interfaces4.go
+++ lesson4/interfaces4.go
@@ -32,10 +32,27 @@ func typeAssertion() {
 	// f = i.(float64)
 }
 
+// A type switch is a construct that permits several type assertions in series
+func typeSwitch(i interface{}) {
+	// A type switch compares the type of the value held by the underlying interface
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("%v * 2 is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("Unknown type %T!\n", v)
+	}
+
+}
+
 // An interface that specifies zero methods is known as an empty interface
 func main() {
 	zeroValue()
 	typeAssertion()
+	typeSwitch(21)
+	typeSwitch("Hello")
+	typeSwitch(true)
 }
 
 // Empty interfaces are used by code that handles values of unknown type
