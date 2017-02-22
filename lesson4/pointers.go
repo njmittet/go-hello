2fa8747a0d7df85a406c0c9fb76dc9552bea352c
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-22 07:09:14 +0100

    Add pointer receiver example

---
+++ lesson4/pointers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Coordinate struct {
+	X, Y float64
+}
+
+// With a value receiver, the method operates on a copy of the original struct
+func (c Coordinate) Abs() float64 {
+	return math.Sqrt(c.X*c.X + c.Y*c.Y)
+}
+
+// Methods can be pointer receivers instead of value receivers
+func (c *Coordinate) Scale(f float64) {
+	c.X = c.X * f
+	c.Y = c.Y * f
+}
+
+func main() {
+	c := Coordinate{3, 4}
+	fmt.Println(c.Abs())
+
+	// Methods with pointer receivers can modify the value of the struct they point to
+	c.Scale(10)
+	fmt.Println(c.Abs())
+}
