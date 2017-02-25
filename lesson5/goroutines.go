4d33289f5fe027ea5fba320b1856e5fdc422e17b
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-25 09:06:22 +0100

    Add closing channel example

--- lesson5/goroutines.go
+++ lesson5/goroutines.go
@@ -28,6 +28,18 @@ func sum(s []int, c chan int) {
 	c <- sum
 }
 
+// A sender can close a channel to indicate that no more values will be sent
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		fmt.Printf("Cap: %v, Len: %v\n", cap(c), len(c))
+		x, y = y, x+y
+	}
+	// Closing is only necessary when the receiver must be told there are no more values, e.g loop termination
+	close(c)
+}
+
 // Channels are a typed conduit through for sending and receiving values with the channel operator: <-
 // Unbuffered channels block receivers until data is available and senders until a receiver is available
 func channels() {
@@ -41,7 +53,7 @@ func channels() {
 }
 
 // Channels can be buffered
-// Buffered blocks a sender once the buffer fills up.
+// Buffered blocks a sender once the buffer fills up
 func bufferedChannels() {
 	ch := make(chan int, 2)
 	ch <- 1
@@ -52,9 +64,20 @@ func bufferedChannels() {
 	fmt.Println(<-ch)
 }
 
+// Receivers can test if a channel has been closed by assigning a second parameter to the receive expression: after
+func closeChannel() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c)*2, c)
+	time.Sleep(1 * time.Second)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
 // Goroutines run in the same address space, so access to shared memory must be synchronized
 func main() {
 	basic()
 	channels()
 	bufferedChannels()
+	closeChannel()
 }
