2af08bdf2c24d1f0569d05d12d104d45bb256c60
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-27 22:15:48 +0100

    Add default select example

--- lesson5/select.go
+++ lesson5/select.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // The select statement lets a goroutine wait on multiple communication operations
@@ -19,7 +20,7 @@ func fibonacci(c, quit chan int) {
 	}
 }
 
-func basicSelect(){
+func basicSelect() {
 	c := make(chan int)
 	quit := make(chan int)
 	// Anonymous function
@@ -32,6 +33,25 @@ func basicSelect(){
 	fibonacci(c, quit)
 }
 
+// The default case in a select is run if no other case is ready
+func defaultSelect() {
+	tick := time.Tick(500 * time.Millisecond)
+	quit := time.After(1500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("Tick")
+		case <-quit:
+			fmt.Println("Quit")
+			return
+		default:
+			fmt.Println(".")
+			time.Sleep(250 * time.Millisecond)
+		}
+	}
+}
+
 func main() {
 	basicSelect()
+	defaultSelect()
 }
